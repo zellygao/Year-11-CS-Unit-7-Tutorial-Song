775b0a5717ffbc2c475694a1e42763b155f014eb
Adam <adam.ewing.nz@gmail.com>
2023-02-23 08:08:51 +0800

    Edited so that SongTester uses JUnit4 to run test cases.

--- src/SongTester.java
+++ src/SongTester.java
@@ -1,24 +1,27 @@
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
 
 public class SongTester {
 
-    Song song = new Song("Lavender Haze", "Taylor Swift");
+    private Song song = new Song("Lavender Haze", "Taylor Swift");
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void TestForTitleAndArtist() {
         assertEquals("Wrong title!", "Lavender Haze", song.getTitle());
         assertEquals("Wrong artist!", "Taylor Swift", song.getArtist());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void TestForDataEncapsulation() {
         assertEquals("Make sure that your attributes are private!", 0, song.getClass().getFields().length);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void TestHowManyMethod() {
         assertEquals(5, song.howMany(new ArrayList<String>(Arrays.asList("Jonathan", "Sylvia", "Rena", "Teri", "Ann Ling"))));
         assertEquals(2, song.howMany(new ArrayList<String>(Arrays.asList("JoNathan", "Renita", "Amanda"))));
