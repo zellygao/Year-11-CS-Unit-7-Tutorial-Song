36a872472081bb6c53735c98a40759c9e80a299a
zellygao <zg2311@student.cis.edu.hk>
2024-02-08 22:01:01 +0900

    ??

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,31 @@
-public class Song {
 
-    private String
+import java.util.ArrayList;
+public class Song {
+    private String title;
+    private String artist;
+    private ArrayList <String> listeners;
+
+    public Song (String title, String artist){
+        this.title = title;
+        this.artist = artist;
+        this.listeners = new ArrayList<>();
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public String getArtist(){
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> newListeners){
+        int newCount = 0;
+        for(String listener: newListeners) {
+            if (!listener.contains(newListeners.toLowerCase())) {
+                listeners.add(listener.toLowerCase());
+                newCount++;
+            }
+        }
+        return newCount;
+    }
 }
